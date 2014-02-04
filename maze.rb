0aba0359e04e69fb11071d9561bf96ecec3577ea
Steven H <stevenhh@brandeis.edu>
2014-02-04 10:33:16 -0500

    2/4/14 - finished solve, trace and (maybe) redesign left

--- maze.rb
+++ maze.rb
@@ -9,31 +9,63 @@ class Maze
 		@cell_col = m
 		@wall_row = (n*2) + 1
 		@wall_col = (m*2) + 1
-		@maze = ""
+		@maze = Hash.new
+		@maze_string = ""
 	end
 
 	def load(arg)
 		# will add a check to see if string is of valid form (wall_row * wall_col)
-		@maze = arg
+		counter = 0
+		arg.split("").each do |i|
+			@maze[[counter / @wall_row, counter % @wall_row]] = i.to_i
+			counter += 1
+		end
+		@maze_string = args
 	end
 
 	def display
-		display = @maze.chars.to_a
+		display = @maze_string.chars.to_a
 		# might refactor
-		for x in 0..((@maze.length)-1)
+		for x in 0..((@maze_string.length)-1)
 			if x % wall_row
 				put "#{display[x..(wall_row - 1)]}"
 			end
 		end
 	end
 
-	def solve
-		# will be implementing with queues. probably going depth-first search approach.
+	def solve(begX, begY, endX, endY)
+		temp = []
+		temp.push([(begX * 2) + 1,(begY * 2) + 1])
+		while temp.length != 0
+			pop = temp.pop
+			if pop == [(endX * 2) + 1,(endY * 2) + 1]
+				return true
+			end
+			neighbors = get_neighbors(pop[0],pop[1])
+			if neighbors.length > 0
+				neighbors.each do |x,y|
+					temp.push([x,y])
+				end
+			end
+		end
+		return false
 	end
 
-	def trace
-		# most likely gonna have the work done in solve and trace will just be a printing 
-		# method. will mark off the successful path with x's
+	def get_neighbors(x, y)
+		temp = []
+		if @maze[x+1,y] == 0
+			temp << [x+1,y]
+		if @maze[x,y+1] == 0
+			temp << [x,y+1]
+		if @maze[x-1,y] == 0
+			temp << [x-1,y]
+		if @maze[x-1,y-1] == 0
+			temp << [x-1,y-1]			
+		return temp
+	end
+
+	def trace(begX, begY, endX, endY)
+		# will think of something witty later
 	end
 
 	def redesign
